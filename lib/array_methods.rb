55cdacd697059d13b93aa04b4bad82dfe8f48a2e
Jack Cole <123jack.cole@gmail.com>
2020-01-09 23:09:59 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -2,7 +2,7 @@ def find_element_index(array, value_to_find)
   #index = 0
   array.length.times do |index|
     if array[index] == value_to_find
-      return i
+      return index
     end
     #index += 1
   end
