6a3ec0a1dfa40d5a7e0fb0ef166b6efb43bd5d27
Jack Cole <123jack.cole@gmail.com>
2020-01-09 23:11:49 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,18 +1,18 @@
 def find_element_index(array, value_to_find)
-  #index = 0
   array.length.times do |index|
     if array[index] == value_to_find
       return index
     end
-    #index += 1
   end
   return nil
 end
 
 def find_max_value(array)
   max = 0 
-  #array.length.times
-  #if 
+  array.length.times do
+  if 
+    
+  end
 end
 
 def find_min_value(array)
