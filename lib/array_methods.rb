f8d31f3f70471c835de5a965e337f6ed9587b414
Jack Cole <123jack.cole@gmail.com>
2020-01-09 23:14:52 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -18,5 +18,11 @@ def find_max_value(array)
 end
 
 def find_min_value(array)
-  
+  min =  
+  array.length.times do |index|
+    if array[index] > max
+      max = array[index]
+    end
+  end
+  min
 end
